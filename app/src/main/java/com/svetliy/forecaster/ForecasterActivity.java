db5c38e2f774222dd78dcb7b4d7e4d7198d4941e
Andrey Kasatkin <a@svetliy.com>
2015-04-30 11:00:05 -0400

    add OkHttp dependency

--- app/src/main/java/com/svetliy/forecaster/ForecasterActivity.java
+++ app/src/main/java/com/svetliy/forecaster/ForecasterActivity.java
@@ -12,28 +12,14 @@ public class ForecasterActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecaster);
+
+        String apiKey = "187ace138d2a8aa7c996cfe74c5c2eb1";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
+                "/" + latitude + "," + -longitude;
+       
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_forecaster, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
